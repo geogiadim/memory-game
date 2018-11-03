8cf5b2cdd80ae4dbbd294eb29e73a92d4d33a626
Xiphunon <gxristidis@gmail.com>
2018-11-03 12:19:46 +0200

    Changed Class ```Card``` logic to store if card is paired(if it has been picked) Minor fixes to Class ```Table```

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,14 @@
 public class Card
 {
     private char valueOfCard;
-    private boolean isOpen;
+    private boolean isPaired;
 
     public Card(){
-        isOpen=false;
+        isPaired=false;
     }
 
-    public void setIsOpen(boolean m){isOpen=m;}
-    public boolean getIsOpen(){return isOpen;}
+    public void setPairedTrue(){isPaired=true;}
+    public boolean getIsOpen(){return isPaired;}
 
     public void setValue (char v){valueOfCard=v;}
     public char getValue (){return valueOfCard;}
--- src/Table.java
+++ src/Table.java
@@ -1,8 +1,18 @@
 public class Table
 {
-    public Table (int x,int y);
-    public openCard(int x,int y);
-    public closeCard(int x,int y);
-    public unableCard(int x,int y);
+    public Table (int x,int y){
 
+    }
+
+    public openCard(int x,int y){
+
+    }
+
+    public closeCard(int x,int y){
+
+    }
+
+    public unableCard(int x,int y){
+
+    }
 }
