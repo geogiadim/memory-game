e3122f05ae715d4cc527569fdecaad5d912e7644
Xiphunon <gxristidis@gmail.com>
2018-11-03 15:04:45 +0200

    Changed Class `Card` to store if card paired and if open Minor fixes to Class `Table`

--- src/Card.java
+++ src/Card.java
@@ -1,15 +1,37 @@
 public class Card
 {
     private char valueOfCard;
+    private boolean isOpen;
     private boolean isPaired;
 
     public Card(){
-        isPaired=false;
+        isPaired = false;
+        isOpen = false;
     }
 
-    public void setPairedTrue(){isPaired=true;}
-    public boolean getIsOpen(){return isPaired;}
+    public void setPairedTrue(){
+        isPaired = true;
+    }
 
-    public void setValue (char v){valueOfCard=v;}
-    public char getValue (){return valueOfCard;}
+    public boolean getIsPaired(){
+        return isPaired;
+    }
+
+    //Setter and Getters for isOpen
+    public void setIsOpen(boolean o){
+        isOpen = o;
+    }
+
+    public boolean getIsOpen(){
+        return isOpen;
+    }
+
+    //Setters and Getters for valueOfCard
+    public void setValue(char v){
+        valueOfCard = v;
+    }
+
+    public char getValue(){
+        return valueOfCard;
+    }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,18 +1,20 @@
 public class Table
 {
-    public Table (int x,int y){
+    private Card[][] tableOfCards;
 
+    public Table (int x,int y){
+        tableOfCards = new Card[x][y];
     }
 
     public openCard(int x,int y){
-
+        //send openCard to UI;
     }
 
     public closeCard(int x,int y){
-
+        //send closeCard to UI;
     }
 
-    public unableCard(int x,int y){
-
+    public void unableCard(int x,int y){
+        tableOfCards[x][y].setPairedTrue();
     }
 }
