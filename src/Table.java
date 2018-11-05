13e6d0627d32743df606b6319157226e31ab48aa
geogiadim <geogiadim@csd.auth.gr>
2018-11-05 13:35:49 +0200

    first look at table class

--- src/Table.java
+++ src/Table.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+
 public class Table
 {
-    public Table (int x,int y);
-    public openCard(int x,int y);
-    public closeCard(int x,int y);
-    public unableCard(int x,int y);
 
+    public Table (int x,int y)
+    {
+        public Card[][] table= new  Card[x][y];
+
+    }
+
+    public void openCard(int x,int y);
+    public void closeCard(int x,int y);
+    public void unableCard(int x,int y);
 }
