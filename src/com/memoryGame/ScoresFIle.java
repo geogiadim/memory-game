4d27d0114080a709c5d80d6c4577bd6312dc1794
geogiadim <geogiadim@csd.auth.gr>
2019-01-18 16:01:36 +0200

    method for file multi player mode

--- src/com/memoryGame/ScoresFIle.java
+++ src/com/memoryGame/ScoresFIle.java
@@ -1,18 +1,23 @@
 package com.memoryGame;
 import com.memoryGame.GUI.GUIConnectionToLogic;
 import java.io.*;
+import java.util.HashMap;
 
 class ScoresFIle implements Serializable{
     private String name;
     private int steps=0,wins=0,mode;
     private String[][] highScores;
     private boolean winner=false;
-    private boolean flag=false;
+    private boolean fileNotExists;
+    private HashMap<String,Integer> hashMap;
 
+    private final String FILE_WINS_BIN= "Memory-Game-Wins.bin";
+    private final String FILE_WINS_TXT = "Memory-Game-Wins.txt";
     private final String FILE_BIN = "Memory-Game-Scores.bin";
     private final String FILE_TXT = "Memory-Game-Scores.txt";
 
     ScoresFIle(String name,int steps,int mode){
+        highScores= new String[7][5];
         this.name=name;
         this.steps=steps;
         this.mode=mode;
@@ -20,6 +25,8 @@ class ScoresFIle implements Serializable{
         createFile();
     }
     ScoresFIle(String name,int mode, boolean isThereWinner){
+        highScores= new String[7][5];
+        hashMap= new HashMap<>();
         this.name=name;
         this.mode=mode;
         winner=isThereWinner;
@@ -28,18 +35,28 @@ class ScoresFIle implements Serializable{
     }
 
     private void checkIfFIleExists(){
-        highScores= new String[7][4];
         if ((new File(FILE_TXT).isFile())){
             System.out.println("txt exists");
+            fileNotExists=false;
             loadFromBinaryFile();
             changeHighScores();
         } else {
             System.out.println("txt not");
-            flag=true;
+            fileNotExists=true;
             initHighScores();
             changeHighScores();
         }
+    }
 
+    private void checkForWins(){
+        if ((new File(FILE_WINS_BIN).isFile())){
+            System.out.println("bin exists");
+            loadWinsFromBinaryFile();
+            changeHashMap();
+        }else {
+            System.out.println("bin not exists");
+            changeHashMap();
+        }
     }
 
     private void initHighScores(){
@@ -51,14 +68,16 @@ class ScoresFIle implements Serializable{
         highScores[5][0] = "Triple Game high score: ";
         highScores[6][0] = "Duel Game high score: ";
         for (int i=0;i<3;i++){
-            highScores[i][1] = "none, ";
-            highScores[i][2] = String.valueOf(0);
-            highScores[i][3] = " steps";
+            highScores[i][1] = "none";
+            highScores[i][2] = ", ";
+            highScores[i][3] = String.valueOf(0);
+            highScores[i][4] = " steps";
         }
         for (int i=3;i<7;i++){
-            highScores[i][1] = "none, ";
-            highScores[i][2] = String.valueOf(0);
-            highScores[i][3] = " wins";
+            highScores[i][1] = "none";
+            highScores[i][2] = ", ";
+            highScores[i][3] = String.valueOf(0);
+            highScores[i][4] = " wins";
         }
     }
 
@@ -86,51 +105,65 @@ class ScoresFIle implements Serializable{
         }
     }
 
+    private void loadWinsFromBinaryFile(){
+        try (DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(FILE_WINS_BIN)))){
+            for (String n : hashMap.keySet()){
+                hashMap.put(in.readUTF(),hashMap.get(n));
+                System.out.println(1);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void saveWinsToBinaryFile() {
+        try(DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(FILE_WINS_BIN)))){
+            for (String name : hashMap.keySet()){
+                out.writeUTF(name);
+                out.write(hashMap.get(name));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void changeHashMap (){
+        boolean flag = false;
+        for (String n : hashMap.keySet()) {
+            if (hashMap.containsKey(name)) {
+                System.out.println("change");
+                hashMap.replace(n, hashMap.get(n) + 1);
+                flag=true;
+            }
+        }
+
+        if (!flag) {
+            hashMap.put(name,1);
+        }
+
+    }
+
     private void changeHighScores(){
-        if (true/*GUIConnectionToLogic.getNumOfPlayers()==1*/){
-            /*switch (mode){
-                case 1:
-                    highScores[0][1]= name+", ";
-                    highScores[0][2]= String.valueOf(steps);
-                    break;
-                case 2:
-                    highScores[1][1]= name+", ";
-                    highScores[1][2]= String.valueOf(steps);
-                    break;
-                case 3:
-                    highScores[2][1]= name+", ";
-                    highScores[2][2]= String.valueOf(steps);
-                    break;
-            }*/
-            if (steps <= Integer.parseInt(highScores[mode-1][2]) || flag || Integer.parseInt(highScores[mode-1][2]) == 0){
-                highScores[mode-1][1]= name+", ";
-                highScores[mode-1][2]= String.valueOf(steps);
+        if (false/*GUIConnectionToLogic.getNumOfPlayers()==1*/){
+            if (steps <= Integer.parseInt(highScores[mode-1][3]) || fileNotExists || Integer.parseInt(highScores[mode-1][3]) == 0){
+                highScores[mode-1][1]= name;
+                highScores[mode-1][3]= String.valueOf(steps);
             }
         }
         else {
             //change the array if there is a winner
             if (winner) {
-                wins++;
-                /*switch (mode){
-                    case 1:
-                        highScores[3][1]= name+", ";
-                        highScores[3][2]= String.valueOf(wins);
-                        break;
-                    case 2:
-                        highScores[4][1]= name+", ";
-                        highScores[4][2]= String.valueOf(wins);
-                        break;
-                    case 3:
-                        highScores[5][1]= name+", ";
-                        highScores[5][2]= String.valueOf(wins);
-                        break;
-                    case 4:
-                        highScores[6][1]= name+", ";
-                        highScores[6][2]= String.valueOf(wins);
-                        break;
-                }*/
-                highScores[mode+2][1]= name+", ";
-                highScores[mode+2][2]= String.valueOf(wins);
+                checkForWins();
+                if (name.equals(highScores[mode+2][1])){
+                    wins = Integer.parseInt(highScores[mode+2][3]) + 1;
+                    highScores[mode+2][3]= String.valueOf(wins);
+                }else {
+                    if (hashMap.get(name)>= Integer.parseInt(highScores[mode+2][3]) || fileNotExists){
+                        highScores[mode+2][1]= name;
+                        highScores[mode+2][3]= String.valueOf(hashMap.get(name));
+                    }
+                }
             }
         }
     }
@@ -138,18 +171,33 @@ class ScoresFIle implements Serializable{
     private void createFile(){
         try (FileWriter writer= new FileWriter(FILE_TXT)){
             for (int i=0; i<7; i++){
-                for (int j=0; j<4; j++){
+                for (int j=0; j<5; j++){
                     writer.write(highScores[i][j]);
                 }
                 writer.write("\n\n");
             }
         }catch (IOException e) {
-            System.err.println("Error");
+            e.printStackTrace();
         }
+
+        try (FileWriter writer= new FileWriter(FILE_WINS_TXT)){
+            for (String n : hashMap.keySet()){
+                writer.write(n);
+                String wins = String.valueOf(hashMap.get(n));
+                writer.write(wins);
+                writer.write("\n\n");
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        if (true/*GUIConnectionToLogic.getNumOfPlayers()>1*/) saveWinsToBinaryFile();
         saveToBinaryFile();
     }
 
     public static void main(String[] args) {
-        ScoresFIle file = new ScoresFIle("nikos",22,1);
+        //ScoresFIle file = new ScoresFIle("nikos",26,3);
+        ScoresFIle file = new ScoresFIle("dimitris",3,true);
     }
 }
